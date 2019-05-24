374ecdd6fd7b7805c658e77a8e5a189a1361f5b0
Muhammad Ghalib <mg.gabe108@gmail.com>
2019-05-24 13:46:51 +1000

    Auto stash before merge of "master" and "origin/master"

---
+++ RadioactiveAmishManiac/Assets/Scripts/PlayerDeathPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeathPoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController pc = other.GetComponent<PlayerController>();
+        pc.health = 0;
+    }
+}
