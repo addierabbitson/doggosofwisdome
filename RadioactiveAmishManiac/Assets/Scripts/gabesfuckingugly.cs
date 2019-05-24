d2abfb13f77556ba51388bdc13fab13bcdb6836b
Cameron Brown <c@meronbrown.com>
2019-05-24 13:57:15 +1000

    score stuff lol

--- RadioactiveAmishManiac/Assets/Scripts/gabesfuckingugly.cs
+++ RadioactiveAmishManiac/Assets/Scripts/gabesfuckingugly.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class gabesfuckingugly : MonoBehaviour
 {
     public static gabesfuckingugly Instance;
+
+    public List<TextMeshProUGUI> scoreTexts;
+    public List<TextMeshProUGUI> bestTexts;
 
     private void Awake()
     {
@@ -18,5 +22,22 @@ public class gabesfuckingugly : MonoBehaviour
         }
 
         DontDestroyOnLoad(this.gameObject);
+        UpdateBestTexts();
     }
+
+    public void SetScore(int score)
+    {
+        foreach (var t in scoreTexts)
+            t.text = "SCORE: " + score.ToString();
+        UpdateBestTexts();
+    }
+
+    void UpdateBestTexts()
+    {
+        int best = PlayerPrefs.GetInt("highscore", 0);
+        foreach (var t in bestTexts)
+            t.text = "BEST: " + best.ToString();
+    }
+
+
 }
